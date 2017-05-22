7960a1c93bfacce509c5103182013761265575ad
beom\hansb <aronix@naver.com>
2017-05-22 16:47:40 +0900

    Constatns 클래스 업데이트

--- javaSwing-Server_Client/src/aboullaite/Constants.java
+++ javaSwing-Server_Client/src/aboullaite/Constants.java
@@ -3,4 +3,9 @@ package aboullaite;
 public class Constants {
 	static final int TYPE_REGISTER=0;
 	static final int TYPE_LOGIN =1;
+	static final String LOGIN_SUCCESS = "loginSuccess";
+	static final String LOGIN_FAILED = "loginFail";
+	static final String REGISTER_SUCCESS = "registerSuccess";
+	static final String REGISTER_FAIL_ID= "registerFailIdExist";
+	static final String REGISTER_FAIL_PASSWD= "registerFailPasswdDif";
 }
