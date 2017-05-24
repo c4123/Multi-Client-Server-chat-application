c5ec7dbc39e741bb9b091b23fec3dcbf42db8fd1
beom\hansb <aronix@naver.com>
2017-05-24 15:27:27 +0900

    Data 클래스, User 클래스 수정

--- javaSwing-Server_Client/src/aboullaite/Data.java
+++ javaSwing-Server_Client/src/aboullaite/Data.java
@@ -4,39 +4,73 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Data implements Serializable{
-	private int type; //1이면 메세지 2이면 유저리스트
+	private int type; 
 	private String msg;
-	private ArrayList<CurrentUser> userList;
+	private String sendor;
+	private String receiver;
+	private ArrayList<User> userList;
 	
-	public Data(int type, String msg, ArrayList<CurrentUser> userList) {
+	public Data(int type, String msg, ArrayList<User> userList) {
 		super();
 		this.type = type;
 		this.msg = msg;
 		this.userList = userList;
 	}
 	
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
-	public String getMsg() {
-		return msg;
-	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-	public ArrayList<CurrentUser> getUserList() {
-		return userList;
-	}
-	public void setUserList(ArrayList<CurrentUser> userList) {
-		this.userList = userList;
-	}
-	
+
 	@Override
 	public String toString() {
 		return "Data [type=" + type + ", msg=" + msg + ", userList=" + userList + "]";
+	}
+
+
+	public int getType() {
+		return type;
+	}
+
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+
+	public String getMsg() {
+		return msg;
+	}
+
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+
+	public String getSendor() {
+		return sendor;
+	}
+
+
+	public void setSendor(String sendor) {
+		this.sendor = sendor;
+	}
+
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+
+	public ArrayList<User> getUserList() {
+		return userList;
+	}
+
+
+	public void setUserList(ArrayList<User> userList) {
+		this.userList = userList;
 	}
 	
 	
--- javaSwing-Server_Client/src/aboullaite/User.java
+++ javaSwing-Server_Client/src/aboullaite/User.java
@@ -1,6 +1,8 @@
 package aboullaite;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 	private String id;
 	private String nickname;
 
