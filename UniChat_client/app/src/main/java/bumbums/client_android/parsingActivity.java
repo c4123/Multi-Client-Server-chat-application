8952ec9446a3cde7d042712384c013d98af5154d
DESKTOP-495FN4C\haechan <c4123@naver.com>
2017-05-31 18:58:38 +0900

    jericho.jre 라이브러리 파일 추가 및  parsingActivity.java  parsing.xml 파일 소스수정.

--- UniChat_client/app/src/main/java/bumbums/client_android/parsingActivity.java
+++ UniChat_client/app/src/main/java/bumbums/client_android/parsingActivity.java
@@ -20,18 +20,26 @@ public class parsingActivity extends AppCompatActivity {
     private static Thread thread = null;
     String parsing_url; //파싱해올 학식 페이지 URL
     String get_data; // 파싱해서 가져온 데이터 저장하는 변수
+    String get_cornerdata;
+    String get_menudata;
     ArrayList<String> array;
-    private TextView txt_text;
+    ArrayList<String> arraycorner;
+    ArrayList<String> arraymenu;
+    private TextView txt_resname;
+    private TextView txt_cornername;
+    //private TextView txt_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.parsing);
 
         parsing_url = "https://dgucoop.dongguk.edu/store/store.php?w=4&l=1";
 
-        txt_text = (TextView)findViewById(R.id.txt_text);  //학식을 보여줄 텍스트뷰
+        txt_resname = (TextView)findViewById(R.id.txt_resname);  //학식을 보여줄 텍스트뷰
+        txt_cornername = (TextView)findViewById(R.id.txt_cornername);
+        //txt_menu = (TextView)findViewById(R.id.txt_menu);
 
         Runnable task = new Runnable() {
             @Override
@@ -48,41 +56,76 @@ public class parsingActivity extends AppCompatActivity {
         } catch (Exception e){
 
         }
-        for (int i=0;i<array.size();i++){
-            txt_text.append(array.get(i) + " "); //쓰레드 작업 끝나면 텍스트뷰에 가져온 데이터 뿌려줌
-    }
+        /*for (int i=0;i<array.size();i++){
+            txt_resname.append(array.get(i) + "\n"); //쓰레드 작업 끝나면 텍스트뷰에 가져온 데이터 뿌려줌
+        }
+        */
+        /*for (int i=0;i<arraycorner.size();i++){
+            txt_cornername.append(arraycorner.get(i) + "\n"); //쓰레드 작업 끝나면 텍스트뷰에 가져온 데이터 뿌려줌
+        }
+        */
+        for (int i=0;i<arraymenu.size();i++){
+            txt_cornername.append(arraymenu.get(i) + "\n"); //쓰레드 작업 끝나면 텍스트뷰에 가져온 데이터 뿌려줌
+        }
     }
 
     public ArrayList<String>getData(String strURL)
     {
         Source source;
         get_data = "";
+        get_cornerdata = "";
+        get_menudata = "";
         array = new ArrayList();
+        arraycorner = new ArrayList();
+        arraymenu = new ArrayList();
         try {
             URL url = new URL(strURL);
-            source = new Source(url); // data넣거나 web주소 입력해서 source instance를 만들면 파싱준비 끝.
+            source = new Source(url); // data넣거나 web주소 입력해서 객체를 만들면 파싱준비 끝.
             Element element = null;
 
-            List<Element> list = source.getAllElements(HTMLElementName.TD); //TD 태그의 엘리먼트를 가져온다.
+            List<Element> list = source.getAllElements(HTMLElementName.TD); // 식당이름 가져오는 객체
 
-                for (int i = 0; i < list.size(); i++) {
+            for (int i = 0; i < list.size(); i++)
+            {
                 element = list.get(i);
                 String sikdang = element.getAttributeValue("class"); // TD 태그의 속성값이 class를  찾는다
-                //String menuname = element.getAttributeValue("align");
+                String menuname = element.getAttributeValue("align");
 
-                if (sikdang != null ) {
-                    if (sikdang.equalsIgnoreCase("menu_st") ||  (sikdang.equalsIgnoreCase("mft style2") )) { // 식당이름,식당내 코너이름,식단을 가져온다.
+                if (sikdang != null )
+                {
+                    if (sikdang.equalsIgnoreCase("menu_st")) //||   )) // 식당이름을 가져온다.
+                    {
                         TextExtractor textExtractor = element.getTextExtractor(); // 해당 문자값을 가져온다.
                         get_data = textExtractor.toString(); // 가져온 값을 스트링으로 변환후
                         array.add(get_data); // Arraylist에 추가한다.
                     }
                 }
+
+                if (sikdang != null )
+                {
+                    if (sikdang.equalsIgnoreCase("mft style2")) //||   )) // 식당내 코너이름을 가져온다.
+                    {
+                        TextExtractor textCornerName = element.getTextExtractor(); // 해당 문자값을 가져온다.
+                        get_cornerdata = textCornerName.toString(); // 가져온 값을 스트링으로 변환후
+                        arraycorner.add(get_cornerdata); // Arraylist에 추가한다.
+                    }
+                }
+
+                if (sikdang != null )
+                {
+                    if (sikdang.equalsIgnoreCase("ft2")) //||   )) // 식당내 메뉴를 가져온다.
+                    {
+                        TextExtractor textMenu = element.getTextExtractor(); // 해당 문자값을 가져온다.
+                        get_menudata = textMenu.toString(); // 가져온 값을 스트링으로 변환후
+                        arraymenu.add(get_menudata); // Arraylist에 추가한다.
+                    }
+                }
+
             }
         } catch (Exception e) {
 
         }
-            return array; //입력된 배열값 리턴
-        }
+        return array; //입력된 배열값 리턴
     }
-
+}
 
