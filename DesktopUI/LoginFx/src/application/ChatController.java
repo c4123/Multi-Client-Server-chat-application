41cb9c7fc4ee94d5e0e017f5df3d37b6dea7c5f0
SeungHoon00 <hansei_xt@naver.com>
2017-05-31 10:33:51 +0900

    Desktop UI ChatScreen ID update

--- DesktopUI/LoginFx/src/application/ChatController.java
+++ DesktopUI/LoginFx/src/application/ChatController.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import application.Resize;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ChatController {
-	/* @FXML private TextArea messageBox;
+	@FXML private Button msgsend;
+	@FXML private ScrollPane userlist;
+	@FXML private TextField messagebox;
+	@FXML private ListView chatscreen;
+	/* 
 	 
 	 public void sendButtonAction() throws IOException {
 	        String msg = messageBox.getText();
